49d6914eda209b6dd0dea336e58a2bb9a43165f2
jeanpierPol <je5x.1.jnes@gmail.com>
2025-05-21 12:18:37 +0200

    creacion del contexto del tema de la pagina

--- frontend/src/components/navbar/ToggleTheme.jsx
+++ frontend/src/components/navbar/ToggleTheme.jsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useTheme } from '../../context/ThemeContext';
 
-export default function ToggleTheme() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    const prefersDark = savedTheme === 'dark';
-    setDarkMode(prefersDark);
-    document.documentElement.setAttribute('data-bs-theme', prefersDark ? 'dark' : 'light');
-  }, []);
-
-  const toggleTheme = () => {
-    const newDarkMode = !darkMode;
-    setDarkMode(newDarkMode);
-    const newTheme = newDarkMode ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
-  };
+export default function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme();
+  const lightIcon = "bi-sun";
+  const darkIcon = "bi-moon";
 
   return (
     <button className="btn btn-outline-secondary" onClick={toggleTheme}>
-      <i className={`bi ${darkMode ? 'bi-sun' : 'bi-moon'}`}></i>
+      <i className={`bi ${theme === 'dark' ? lightIcon : darkIcon }`}></i>
     </button>
   );
 }
